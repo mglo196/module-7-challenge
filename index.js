ae9980338165889b5fa5d683dcabb80aafd9508b
Margaux Glovier <margauxglovier@gmail.com>
2024-11-04 13:05:57 -0500

    changes to index.js

--- index.js
+++ index.js
@@ -1,13 +1,92 @@
 // TODO: Include package needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the purpose of this project?",
+    "What is the title of this project?",
+    "Who are the creators of this project?",
+    "What are the licenses and terms of use for this project?",
+    "How do you install this project?",
+    "How can this project be improved in the future?",
+    "How do you report bugs in this project?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md generated')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'Purpose',
+           },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'Title',
+           },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'Creators',
+           },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'Licenses',
+           },
+           {
+            type: 'input',
+            message: questions[4],
+            name: 'Install',
+           },
+           {
+            type: 'input',
+            message: questions[5],
+            name: 'Improvements',
+           },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'Bugs',
+           }
+    ])
+.then((answers => {
+    const {Purpose, Title, Creators, Licenses, Install, Improvements, Bugs} = answers;
+
+    const layout = `
+    # ${Title}
+    
+    ## Description
+    ${Purpose}
+    
+    ## Creators
+    ${Creators}
+    
+    ## License
+    ${Licenses}
+    
+    ## Installation
+    ${Install}
+    
+    ## Future Improvements
+    ${Improvements}
+    
+    ## Reporting Bugs
+    ${Bugs}
+    `;
+    
+            writeToFile('README.md', layout);
+        }));
+    }
 
 // Function call to initialize app
 init();
